a97008df77a855b851daf0c79212cfe95b0075ec
adityofr9 <adityofr9@gmail.com>
2022-07-07 09:24:27 +0700

    Memunculkan tulisan pada command

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ const fs = require('fs');
 //console.log(fs);
 
 //menuliskan string ke file (Synchronous)
-fs.writeFileSync('test.txt', 'Hellow World secara synchronous!');
+//fs.writeFileSync('test.txt', 'Hellow World secara synchronous!');
+fs.readFile('test.txt', 'utf-8', (err,data) => {
+    if (err) throw err;
+    console.log(data);
+});
