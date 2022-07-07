ac4b219ea1a6a5b0c037c0250524a410ab534767
adityofr9 <adityofr9@gmail.com>
2022-07-07 09:31:04 +0700

    menggunakan modules Readline

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-//core module
-//file system
+//CORE MODULE
+//File System
 const fs = require('fs');
+//Readline
+const readline = require('readline');
 
 //console.log(fs);
 
 //menuliskan string ke file (Synchronous)
 //fs.writeFileSync('test.txt', 'Hellow World secara synchronous!');
-fs.readFile('test.txt', 'utf-8', (err,data) => {
-    if (err) throw err;
-    console.log(data);
+// fs.readFile('test.txt', 'utf-8', (err,data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+//Latihan Readline
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('What is your name? ', (name) => {
+    console.log(`Thank you ${name}`);
+
+    rl.close();
 });
